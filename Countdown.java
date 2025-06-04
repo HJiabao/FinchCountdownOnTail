0259497a7ce5d163b23e570666bed07bc2f3684b
HJiabao <Jiabaoh2@nycstudents.net>
2025-06-04 09:58:37 -0400

    Update Countdown.java

--- Countdown.java
+++ Countdown.java
@@ -1,3 +1,30 @@
-public class Countdown{
-  
+public void times(int time){
+  for(int i=0;i<time;i++){
+    for(int j=0;j<11;j++){
+      numbers(j);
+      bird.pause(1);
+    }
+  }
+}
+
+public void numbers(int num){
+  int[] display= {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  switch(num){
+    case 1:
+      display={0,0,1,0,0,0,1,1,0,0,1,0,1,0,0,0,0,1,0,0,1,1,1,1,1};
+      bird.setDisplay(display);
+      break;
+    case 2:
+      display={0,1,1,1,0,1,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,1,1,1,1};
+      bird.setDisplay(display);
+      break;
+    case 3:
+      display={0,1,1,1,0,1,0,0,0,1,0,0,1,1,0,1,0,0,0,1,0,1,1,1,0};
+      bird.setDisplay(display);
+      break;
+    case 4:
+      display={1,0,1,0,0,1,0,1,0,0,1,1,1,1,1,0,0,1,0,0,0,0,1,0,0};
+      bird.setDisplay(display);
+      break;
+  }
 }
